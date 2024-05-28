634d9b718430077d16d791273a2583a08966d9d6
Cory Dransfeldt <hi@coryd.dev>
2024-05-28 09:06:50 -0700

    chore: simplify logic

--- select-pagination.js
+++ select-pagination.js
@@ -1,64 +1,48 @@
 class SelectPagination extends HTMLElement {
-  static register(tagName) {
-    if ("customElements" in window) {
-      customElements.define(tagName || "select-pagination", SelectPagination);
-    }
+  static register(tagName = 'select-pagination') {
+    if ("customElements" in window) customElements.define(tagName, this)
   }
 
-  static attr = {
-    baseIndex: 'data-base-index',
+  static get observedAttributes() {
+    return ['data-base-index']
   }
 
   get baseIndex() {
-    return this.getAttribute(SelectPagination.attr.baseIndex) || 0
+    return this.getAttribute('data-base-index') || 0
   }
 
   connectedCallback() {
     if (this.shadowRoot) return
 
-    let shadowRoot = this.attachShadow({ mode: 'open' })
-    let slot = document.createElement('slot')
-    shadowRoot.appendChild(slot)
+    this.attachShadow({ mode: 'open' }).appendChild(document.createElement('slot'))
 
-    const uri = window.location.pathname
-    const urlSegments = uri.split('/').filter(segment => segment !== '')
-
-    let pageNumber = this.extractPageNumber(urlSegments) || 0
+    const uriSegments = window.location.pathname.split('/').filter(Boolean)
+    let pageNumber = this.extractPageNumber(uriSegments) || 0
 
     this.control = this.querySelector('select')
-    this.control.querySelector(`option[value="${pageNumber.toString()}"]`).setAttribute('selected', 'selected')
+    this.control.value = pageNumber
     this.control.addEventListener('change', (event) => {
-      pageNumber = event.target.value
-
-      if (!isNaN(pageNumber)) {
-        pageNumber = parseInt(pageNumber)
-      }
-
-      const updatedUrlSegments = this.updateUrlSegments(urlSegments, pageNumber)
-      window.location.href = `${window.location.protocol}//${window.location.host}/${updatedUrlSegments.join('/')}`
+      pageNumber = parseInt(event.target.value)
+      const updatedUrlSegments = this.updateUrlSegments(uriSegments, pageNumber)
+      window.location.href = `${window.location.origin}/${updatedUrlSegments.join('/')}`
     })
   }
 
   extractPageNumber(segments) {
-    for (let i = segments.length - 1; i >= 0; i--) {
-      const segment = segments[i]
-      if (!isNaN(segment)) {
-        return parseInt(segment)
-      }
-    }
-    return null
+    const lastSegment = segments[segments.length - 1]
+    return !isNaN(lastSegment) ? parseInt(lastSegment) : null
   }
 
   updateUrlSegments(segments, pageNumber) {
-    if (segments.length > 0 && !isNaN(segments[segments.length - 1])) {
+    if (!isNaN(segments[segments.length - 1])) {
       segments[segments.length - 1] = pageNumber.toString()
     } else {
       segments.push(pageNumber.toString())
     }
-    if (pageNumber === parseInt(this.baseIndex)) segments.pop()
 
-    return segments;
+    if (pageNumber === parseInt(this.baseIndex)) segments.pop()
+    return segments
   }
 }
 
-SelectPagination.register();
+SelectPagination.register()
